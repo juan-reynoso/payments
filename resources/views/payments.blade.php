ace836707baffa867af165be6bb2260c948f4ada
Juan Reynoso Elias <juan@ikkiware.com>
2024-07-04 12:05:55 -0600

    Inital version

---
+++ resources/views/payments.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eliminar Pagos en Bloque</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+</body>
+<div class="container mt-5">
+<h1 class="text-center">Payments</h1>
+</div>
+</html>
